b8f45b172bd13cd997a285b799c2bb005ed44516
Matt Holland <matt.holland@myunidays.com>
2019-12-14 20:10:11 +0000

    Implements day 5 part 1.

--- 2019/intcode/intcodemachine.go
+++ 2019/intcode/intcodemachine.go
@@ -2,6 +2,8 @@ package intcode
 
 type Machine struct {
 	memory []int
+	input  int
+	output []int
 }
 
 func NewIntcodeMachine() *Machine {
@@ -22,18 +24,68 @@ func (m *Machine) LoadMemory(memory []int) {
 func (m *Machine) RunProgram() {
 	ip := 0
 	for {
-		opCode := m.memory[ip]
-		switch opCode {
+		instruction := parseInstruction(m.memory[ip])
+		switch instruction.opCode {
 		case 1:
-			m.memory[m.memory[ip+3]] = m.memory[m.memory[ip+1]] + m.memory[m.memory[ip+2]]
+			m.memory[m.memory[ip+3]] = m.GetOperand(ip+1, instruction.parameterOneMode) + m.GetOperand(ip+2, instruction.parameterTwoMode)
+			ip += 4
 		case 2:
-			m.memory[m.memory[ip+3]] = m.memory[m.memory[ip+1]] * m.memory[m.memory[ip+2]]
+			m.memory[m.memory[ip+3]] = m.GetOperand(ip+1, instruction.parameterOneMode) * m.GetOperand(ip+2, instruction.parameterTwoMode)
+			ip += 4
+		case 3:
+			m.memory[m.memory[ip+1]] = m.input
+			ip += 2
+		case 4:
+			m.output = append(m.output, m.GetOperand(ip+1, instruction.parameterOneMode))
+			ip += 2
 		case 99:
 			return
 		}
-		ip += 4
 	}
 }
+
+func (m *Machine) SetInput(input int) {
+	m.input = input
+}
+
+func (m *Machine) GetOutput() []int {
+	return m.output
+}
+
+func (m *Machine) GetOperand(address int, mode int) int {
+	switch mode {
+	case immediateMode:
+		return m.memory[address]
+	default:
+		return m.memory[m.memory[address]]
+	}
+}
+
+func parseInstruction(instruction int) *Instruction {
+	opCode := instruction % 100
+	p1Mode := (instruction / 100) % 10
+	p2Mode := (instruction / 1000) % 10
+	p3Mode := (instruction / 10000) % 10
+
+	return &Instruction{
+		opCode:             opCode,
+		parameterOneMode:   p1Mode,
+		parameterTwoMode:   p2Mode,
+		parameterThreeMode: p3Mode,
+	}
+}
+
+type Instruction struct {
+	opCode             int
+	parameterOneMode   int
+	parameterTwoMode   int
+	parameterThreeMode int
+}
+
+const (
+	positionMode int = iota
+	immediateMode
+)
 
 func (m *Machine) GetMemory() []int {
 	return m.memory
--- 2019/intcode/intcodemachine_test.go
+++ 2019/intcode/intcodemachine_test.go
@@ -59,3 +59,37 @@ func TestRunProgram_ExampleProgramOne(t *testing.T) {
 		t.Errorf("Output incorrect, expected: %v but actual: %v", expected, actual)
 	}
 }
+
+func TestRunProgram_ParameterModeInstructions(t *testing.T) {
+	machine := NewIntcodeMachine()
+	machine.LoadMemory([]int{1002, 4, 3, 4, 33})
+	machine.RunProgram()
+	actual := machine.GetMemory()
+	expected := []int{1002, 4, 3, 4, 99}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Output incorrect, expected: %v but actual: %v", expected, actual)
+	}
+}
+
+func TestRunProgram_InputOutputOpCodes(t *testing.T) {
+	machine := NewIntcodeMachine()
+	machine.LoadMemory([]int{3, 0, 4, 0, 99})
+	machine.SetInput(42)
+	machine.RunProgram()
+	actual := machine.GetOutput()
+	expected := []int{42}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Output incorrect, expected: %v but actual: %v", expected, actual)
+	}
+}
+
+func TestRunProgram_NegativeParameterValues(t *testing.T) {
+	machine := NewIntcodeMachine()
+	machine.LoadMemory([]int{1101, 100, -1, 4, 0})
+	machine.RunProgram()
+	actual := machine.GetMemory()
+	expected := []int{1101, 100, -1, 4, 99}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Output incorrect, expected: %v but actual: %v", expected, actual)
+	}
+}
