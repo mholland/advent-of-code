0cc7b923b54bd6ab0c88581009951b1e8c10981b
Matt Holland <matt.holland90@gmail.com>
2021-12-02 17:54:43 +0000

    Improves day 1.

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,14 +1,12 @@
-lines = []
-with open("inputs/day01.input") as f:
-    lines = [int(i) for i in f]
+depths = [int(i) for i in open("inputs/day01.input")]
 
 increases = 0
 window_increases = 0
-for i in range(len(lines)):
-    if i > 0 and lines[i] > lines[i - 1]:
+for i in range(len(depths)):
+    if i > 0 and depths[i] > depths[i - 1]:
         increases += 1
 
-    if i > 2 and lines[i] > lines[i-3]:
+    if i > 2 and depths[i] > depths[i-3]:
         window_increases += 1
 
 
