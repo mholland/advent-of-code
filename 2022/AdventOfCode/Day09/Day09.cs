a3249e3819fb73238bca72fecfab79504186cb8d
Matt Holland <matt.holland@myunidays.com>
2022-12-12 17:39:24 +0000

    Implements part 2 of day 9.

--- 2022/AdventOfCode/Day09/Day09.cs
+++ 2022/AdventOfCode/Day09/Day09.cs
@@ -21,18 +21,36 @@ public sealed class Day09 : TestBase
         "R 2"
     };
 
-    [Fact]
-    public void ExampleOne() => CountPositionsVisited(Example).Should().Be(13);
-
-    [Fact]
-    public void PartOne() => Output.WriteLine($"Visited: {CountPositionsVisited(Input)}");
-
-    private int CountPositionsVisited(string[] input)
+    private string[] ExTwo = new[]
     {
-        var visited = new HashSet<(int X, int Y)>();
-        var knots = new LinkedList<(int X, int Y)>();
-        knots.AddLast((X: 0, Y: 0));
-        knots.AddLast((X: 0, Y: 0));
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20"
+    };
+
+    [Fact]
+    public void ExampleOne() => CountPositionsVisited(Example, 2).Should().Be(13);
+
+    [Fact]
+    public void PartOne() => Output.WriteLine($"Visited: {CountPositionsVisited(Input, 2)}");
+
+    [Fact]
+    public void ExampleTwo() => CountPositionsVisited(ExTwo, 10).Should().Be(36);
+
+    [Fact]
+    public void PartTwo() => Output.WriteLine($"Visited: {CountPositionsVisited(Input, 10)}");
+
+    private int CountPositionsVisited(string[] input, int knotCount)
+    {
+        var visited = new HashSet<(int X, int Y, int Num)>();
+        var knots = new LinkedList<(int X, int Y, int Num)>();
+        for (var c = 0; c < knotCount; c++)
+            knots.AddLast((X: 0, Y: 0, Num: c));
         visited.Add(knots.Last!.Value);
         var moves = ParseMovements(input);
         var deltas = new Dictionary<string, (int X, int Y)>
@@ -48,19 +66,19 @@ public sealed class Day09 : TestBase
             var head = knots.First!;
             for (var a = 0; a < dist; a++)
             {
-                var prev = (X: head.Value.X, Y: head.Value.Y);
-                head.Value = (X: head.Value.X + delta.X, Y: head.Value.Y + delta.Y);
+                head.Value = (X: head.Value.X + delta.X, Y: head.Value.Y + delta.Y, Num: head.Value.Num);
                 var cur = head.Next;
                 while (cur is not null)
                 {
                     var ahead = cur.Previous!.Value;
                     if (Math.Abs(ahead.X - cur.Value.X) > 1 || Math.Abs(ahead.Y - cur.Value.Y) > 1)
                     {
-                        cur.Value = (X: prev.X, Y: prev.Y);
+                        var c = cur.Value;
+                        var sign = (X: Math.Sign(ahead.X - c.X), Y: Math.Sign(ahead.Y - c.Y));
+                        cur.Value = (X: c.X + sign.X, Y: c.Y + sign.Y, cur.Value.Num);
                         if (cur.Next is null)
                             visited.Add(cur.Value);
                     }
-                    prev = cur.Value;
                     cur = cur.Next;
                 }
             }
