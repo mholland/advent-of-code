60dedc41457f9cd6efa8f01fc14823727e2c14dd
Matt Holland <matt.holland90@gmail.com>
2022-12-13 00:40:03 +0000

    Day 12 part 2.

--- 2022/AdventOfCode/Day12/Day12.cs
+++ 2022/AdventOfCode/Day12/Day12.cs
@@ -22,9 +22,22 @@ public sealed class Day12 : TestBase
     [Fact]
     public void PartOne() => Output.WriteLine($"Shortest: {CalculateShortestDistance(Input)}");
 
-    private int CalculateShortestDistance(string[] input)
+    [Fact]
+    public void ExampleTwo() => CalculateOverallShortestDistance(new[]
     {
-        var grid = new Dictionary<(int X, int Y), int>();
+        "Sabqponm",
+        "abcryxxl",
+        "accszExk",
+        "acctuvwj",
+        "abdefghi"
+    }).Should().Be(29);
+
+    [Fact]
+    public void PartTwo() => Output.WriteLine($"Shortest: {CalculateOverallShortestDistance(Input)}");
+
+    private (IDictionary<(int X, int Y), int> Grid, (int X, int Y) Start, (int X, int Y) End) ParseGrid(string[] input)
+    {
+                var grid = new Dictionary<(int X, int Y), int>();
         (int X, int Y) start = default;
         (int X, int Y) end = default;
         for (var y = 0; y < input.Length; y++)
@@ -45,8 +58,29 @@ public sealed class Day12 : TestBase
             grid.Add(coord, height - 'a' + 1);
         }
 
+        return (grid, start, end);
+    }
+
+    private int CalculateOverallShortestDistance(string[] input)
+    {
+        var (grid, _, end) = ParseGrid(input);
+        var starts = grid.Where(x => x.Value == 1).Select(x => x.Key).ToArray();
+
+        return ShortestDistance(grid, starts, end);
+    }
+
+    private int CalculateShortestDistance(string[] input)
+    {
+        var (grid, start, end) = ParseGrid(input);
+
+        return ShortestDistance(grid, new[] { start }, end);
+    }
+
+    private int ShortestDistance(IDictionary<(int X, int Y), int> grid, (int X, int Y)[] starts, (int X, int Y) end)
+    {
         var queue = new Queue<(int X, int Y, int Dist, int Height)>();
-        queue.Enqueue((start.X, start.Y, 0, 1));
+        foreach (var start in starts)
+            queue.Enqueue((start.X, start.Y, 0, 1));
         var visited = new HashSet<(int X, int Y)>();
         var dirs = new(int X, int Y)[]{ (1, 0), (-1, 0), (0, 1), (0, -1) };
 
