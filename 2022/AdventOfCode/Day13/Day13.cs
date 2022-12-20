428b13e8773553226913704058f6c3d60990707e
Matt Holland <matt.holland90@gmail.com>
2022-12-20 13:07:57 +0000

    Adds day 13 solutions.

--- 2022/AdventOfCode/Day13/Day13.cs
+++ 2022/AdventOfCode/Day13/Day13.cs
@@ -39,12 +39,79 @@ public sealed class Day13 : TestBase
     };
 
     [Fact]
-    public void ExampleOne() => FindCorrectIndices(Example).Should().Be(13);
+    public void ExampleOne() => FindCorrectSignals(Example).Should().Be(13);
 
-    private int FindCorrectIndices(string[] input)
+    [Fact]
+    public void PartOne() => Output.WriteLine($"Signals: {FindCorrectSignals(Input)}");
+
+    [Fact]
+    public void ExampleTwo() => FindDecoderKey(Example).Should().Be(140);
+
+    [Fact]
+    public void PartTwo() => Output.WriteLine($"Decoder key: {FindDecoderKey(Input)}");
+
+    private int FindDecoderKey(string[] input)
     {
-        var chunks = Enumerable.Chunk(input.Where(x => !string.IsNullOrWhiteSpace(x)), 2);
-        var packets = chunks.Select(x => (First: JsonArray.Parse(x[0])!, Second: JsonArray.Parse(x[1])!));
-        return 0;
+        var packets = input.Where(i => !string.IsNullOrWhiteSpace(i))
+            .Select(p => (JsonArray.Parse(p) as JsonArray)!)
+            .ToList();
+
+        var two = new JsonArray(new JsonArray(2));
+        var six = new JsonArray(new JsonArray(6));
+        packets.Add(two);
+        packets.Add(six);
+
+        var sorted = packets
+            .OrderBy(x => x, new PacketComparer())
+            .Select((x, i) => (Packet: x, Ord: i + 1));
+
+        var twoOrd = sorted.FirstOrDefault(s => PacketComparer.CompareTo(s.Packet, two) == 0).Ord;
+        var sixOrd = sorted.FirstOrDefault(s => PacketComparer.CompareTo(s.Packet, six) == 0).Ord;
+
+        return twoOrd * sixOrd;
+    }
+
+    private int FindCorrectSignals(string[] input) =>
+        Enumerable.Chunk(input.Where(i => !string.IsNullOrWhiteSpace(i)), 2)
+            .Select(i => (First: JsonArray.Parse(i[0]) as JsonArray, Second: JsonArray.Parse(i[1]) as JsonArray))
+            .Select((x, i) => (Ord: i + 1, Comp: PacketComparer.CompareTo(x.First!, x.Second!)))
+            .Aggregate(0, (acc, c) => acc += c.Comp == -1 ? c.Ord : 0);
+
+    private sealed class PacketComparer : IComparer<JsonArray>
+    {
+        public int Compare(JsonArray? x, JsonArray? y) =>
+            (x, y) switch
+            {
+                (null, null) => 0,
+                (null, _) => -1,
+                (_, null) => 1,
+                _ => CompareTo(x, y)
+            };
+
+        public static int CompareTo(JsonNode x, JsonNode y)
+        {
+            return (x, y) switch
+            {
+                (JsonValue i1, JsonValue i2) => Math.Sign(i1.GetValue<int>() - i2.GetValue<int>()),
+                (JsonValue i1, JsonArray j2) => CompareTo(new JsonArray(i1.GetValue<int>()), j2),
+                (JsonArray j1, JsonValue i2) => CompareTo(j1, new JsonArray(i2.GetValue<int>())),
+                (JsonArray j1, JsonArray j2) => Cmp(j1, j2),
+                _ => throw new Exception("Unsupported value")
+            };
+
+            int Cmp(JsonArray j1, JsonArray j2) 
+            {
+                int i;
+                for (i = 0; i < Math.Min(j1.Count, j2.Count); i++)
+                {
+                    var cmp = CompareTo(j1[i]!, j2[i]!);
+                    if (cmp == -1 || cmp == 1) return cmp;
+                }
+                if (i == j1.Count && i < j2.Count) return -1;
+                if (i == j2.Count && i < j1.Count) return 1;
+
+                return 0;
+            }
+        }
     }
 }
