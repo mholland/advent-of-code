8b07ff4b2575872eb5b3b52f89dd2f49956ffec0
Matt Holland <matt.holland@myunidays.com>
2024-12-02 23:09:02 +0000

    Adds 2024 day 2 part 2.

--- 2024/AdventOfCode/Day02/Day02.cs
+++ 2024/AdventOfCode/Day02/Day02.cs
@@ -15,26 +15,60 @@ public sealed class Day02(ITestOutputHelper output) : TestBase(output)
     ];
 
     [Fact]
-    public void ExampleOne() => SafeReports(_example, 0).Should().Be(2);
+    public void ExampleOne() => SafeReports(_example).Should().Be(2);
 
     [Fact]
-    public async Task PartOne() => WriteOutput(SafeReports(await ReadInputLines(), 0));
+    public async Task PartOne() => WriteOutput(SafeReports(await ReadInputLines()));
 
-    private static int SafeReports(string[] input, int tolerance)
+    [Fact]
+    public void IndexMusings()
     {
-        var safe = 0;
-        foreach (var line in input)
-        {
-            var report = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            List<int> diffs = [];
-            for (var i = 0; i < report.Length - 1; i++)
-                diffs.Add(report[i] - report[i+1]);
+        int[] foo = [1, 2, 3, 4, 5];
+        foo[..1].Length.Should().Be(1);
+        foo[2..].Length.Should().Be(3);
+        int[] bar = [..foo[..1], ..foo[2..]];
+        bar.Length.Should().Be(4);
+        bar.Should().BeEquivalentTo([1, 3, 4, 5]);
+    }
 
-            if (diffs.Select(x => Math.Sign(x)).Distinct().Count() == 1
-                && diffs.Select(Math.Abs).All(x => x >= 1 && x <= 3))
-                safe += 1;
-        }
+    [Fact]
+    public void ExampleTwo() => SafeReports(_example, true).Should().Be(4);
+
+    [Fact]
+    public async Task PartTwo() => WriteOutput(SafeReports(await ReadInputLines(), true));
+
+    private static int SafeReports(string[] input, bool tolerant = false)
+    {
+        var reports = input
+            .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray())
+            .ToArray();
+        var safe = reports.Count(IsSafe);
+        if (!tolerant) return safe;
+
+        var @unsafe = reports.Where(r => !IsSafe(r));
+        safe += @unsafe
+            .Count(report =>
+                report.Select((_, i) => report.Without(i)).Any(IsSafe));
 
         return safe;
+
+        bool IsSafe(int[] report)
+        {
+            var direction = Math.Sign(report[0] - report[1]);
+            for (var i = 0; i < report.Length - 1; i++)
+            {
+                var diff = Math.Abs(report[i] - report[i + 1]);
+                if (diff is < 1 or > 3) return false;
+                if (Math.Sign(report[i] - report[i + 1]) != direction) return false;
+            }
+
+            return true;
+        }
     }
+}
+
+public static class Extensions
+{
+    public static int[] Without(this int[] self, int index) => [..self[..index], ..self[(index + 1)..]];
 }
